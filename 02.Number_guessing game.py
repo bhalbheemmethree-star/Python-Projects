f42d6023c198bb7d20ff7a24b44c1b2b2c553cc3
bhalbheemmethree-star <bhalbheemmethree@gmail.com>
2026-08-01 22:24:03 +0530

    Number guessing game

---
+++ 02.Number_guessing game.py
@@ -0,0 +1,25 @@
+
+import random
+
+secret_number = random.randint(1, 100)
+attempts = 0
+
+print("================================")
+print("     NUMBER GUESSING GAME       ")
+print("================================")
+print("🎯 Welcome to the Number Guessing Game!")
+print("Guess a number between 1 and 100.")
+
+
+while True:
+    guess = int(input("Enter your guess: "))
+    attempts += 1
+
+    if guess > secret_number :
+        print("Too High! Try again.")
+    elif guess < secret_number:
+        print("Too Low! Try again.")
+    else:
+        print(f"correct! The number was {secret_number}.")
+        print(F"Attempts: {attempts}")
+        break
